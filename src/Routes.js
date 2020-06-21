adbf2aaebba63091aa8ed32ff13f91a3480020e3
Tom Durrant <tomchdurrant@gmail.com>
2020-06-21 13:40:46 +0100

    Setting up react app

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
 
 export default function Routes() {
-    return (
-        <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-        </Switch>
-    )
+	return (
+		<Switch>
+			<Route exact path="/">
+				<Home />
+			</Route>
+			{/* Finally, catch all unmatched routes */}
+			<Route>
+				<NotFound />
+			</Route>
+		</Switch>
+	);
 }
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+	s3: {
+		REGION: "eu-west-2",
+		BUCKET: "notes-app-uploads-tdur",
+	},
+	apiGateway: {
+		REGION: "eu-west-2",
+		URL: "https://irqr4iwvz4.execute-api.eu-west-2.amazonaws.com/prod",
+	},
+	cognito: {
+		REGION: "eu-west-2",
+		USER_POOL_ID: "eu-west-2_lYJdCGgfZ",
+		APP_CLIENT_ID: "7stjj4nb0q0ci3ii295jobu83b",
+		IDENTITY_POOL_ID: "eu-west-2:28af2818-7e2c-4a60-b970-b9eb335a5903",
+	},
+};
